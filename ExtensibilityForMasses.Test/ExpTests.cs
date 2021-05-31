d57b1da2a76b915ced8ad96ace97365ec7d4b490
Mark Seemann <mark@ploeh.dk>
2021-05-31 17:10:57 +0200

    Add test of Lit

---
+++ ExtensibilityForMasses.Test/ExpTests.cs
@@ -0,0 +1,28 @@
+﻿using Ploeh.Study.ExtensibilityForMasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExtensibilityForMasses.Test
+{
+    public sealed class ExpTests
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(00)]
+        [InlineData(01)]
+        [InlineData(11)]
+        public void EvalLit(int x)
+        {
+            var sut = new Lit(x);
+
+            var actual = sut.Eval();
+
+            var vint = Assert.IsAssignableFrom<VInt>(actual);
+            Assert.Equal(x, vint.Int);
+        }
+    }
+}
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-dotnet build --configuration Release
+dotnet test --configuration Release
