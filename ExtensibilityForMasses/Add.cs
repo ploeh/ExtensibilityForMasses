c0700301e5f38943052121e82ba80c689b17125a
Mark Seemann <mark@ploeh.dk>
2021-05-31 09:34:07 +0200

    Add code corresponding to figure 1
    I've chosen to go with C# naming conventions, so interfaces are prefixed
    with `I`, and casing is consistent with C#. Furthermore, I've translated
    Java getters and setters to C# properties.

---
+++ ExtensibilityForMasses/Add.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public class Add : IExp
+    {
+        private readonly IExp l;
+        private readonly IExp r;
+
+        public Add(IExp l, IExp r)
+        {
+            this.l = l;
+            this.r = r;
+        }
+
+        public IValue Eval()
+        {
+            return new VInt(l.Eval().Int + r.Eval().Int);
+        }
+    }
+}
---
+++ ExtensibilityForMasses/Lit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public class Lit : IExp
+    {
+        private readonly int x;
+
+        public Lit(int x)
+        {
+            this.x = x;
+        }
+
+        public IValue Eval()
+        {
+            return new VInt(x);
+        }
+    }
+}
