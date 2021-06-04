e443f93342d5f75b45d7d3b281665f76cfe93801
Mark Seemann <mark@ploeh.dk>
2021-06-04 08:15:00 +0200

    Add IIntBoolAlg<A>
    Following standard .NET naming convention, I've Pascal-cased the method
    names. I'm a little worried that they might collide with the two class
    names, but time will tell...

---
+++ ExtensibilityForMasses/IIntBoolAlg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public interface IIntBoolAlg<A> : IIntAlg<A>
+    {
+        A Bool(bool b);
+        A Iff(A e1, A e2, A e3);
+    }
+}
