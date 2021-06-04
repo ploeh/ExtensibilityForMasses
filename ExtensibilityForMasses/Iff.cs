e9d8fa374957fa586b9d7e4d5712de8a1be355ab
Mark Seemann <mark@ploeh.dk>
2021-06-04 08:09:56 +0200

    Add Bool and Iff classes
    How to actually implement IExp in these to classes escapes me, and the
    paper doesn't say. I've posted a question on Stack Overflow
    (https://stackoverflow.com/q/67818254/126014), but have yet to receive
    an answer.
    
    I'm pressing ahead with two exception-throwing implementations to see
    what happens next in the paper.

---
+++ ExtensibilityForMasses/Iff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public sealed class Iff : IExp
+    {
+        public A Accept<A>(IIntAlg<A> vis)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
