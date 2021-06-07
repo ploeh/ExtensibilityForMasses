8927d0980529c2a1693db11732bd7ab71e5cfea4
Mark Seemann <mark@ploeh.dk>
2021-06-07 09:56:24 +0200

    Add interfaces from figure 10
    No tests yet, because no implementations yet exist.

---
+++ ExtensibilityForMasses/IBoolAlg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public interface IBoolAlg<A>
+    {
+        A Bool(bool b);
+        A Iff(A b, A e1, A e2);
+    }
+}
---
+++ ExtensibilityForMasses/IExpIntBool.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public interface IExpIntBool<A> : IBoolAlg<A>, IIntAlg<A>
+    {
+    }
+}
