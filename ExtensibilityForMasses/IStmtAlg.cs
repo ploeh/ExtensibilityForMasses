4b9b80b2dfdbec852ded888d72eb8faee7a4d40a
Mark Seemann <mark@ploeh.dk>
2021-06-07 08:21:48 +0200

    Add statement algebra from figure 8
    No tests yet, since no executable code was introduced.

---
+++ ExtensibilityForMasses/IStmtAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public interface IStmtAlg<E, S> : IIntBoolAlg<E>
+    {
+        E Var(string x);
+        E Assign(string x, E e);
+        S Expr(E e);
+        S Comp(S e1, S e2);
+    }
+}
