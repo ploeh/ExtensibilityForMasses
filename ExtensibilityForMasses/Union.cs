b8f9305aa6cdf0ca68d3149d751bba7b67900bc8
Mark Seemann <mark@ploeh.dk>
2021-06-08 11:04:22 +0200

    Add Union class
    On page 16, the paper discusses the union of the two interfaces as an
    interface called `IntBoolAlg`, despite figure 10 calling it
    `ExpIntBool`. I'm going with `ExpIntBool` because figure 6 already
    introduced an `IntBoolAlg` interface, which is different.

---
+++ ExtensibilityForMasses/Union.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public class Union<A> : IExpIntBool<A>
+    {
+        private readonly IBoolAlg<A> v1;
+        private readonly IIntAlg<A> v2;
+
+        public Union(IBoolAlg<A> v1, IIntAlg<A> v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public A Lit(int x)
+        {
+            return v2.Lit(x);
+        }
+
+        public A Add(A e1, A e2)
+        {
+            return v2.Add(e1, e2);
+        }
+
+        public A Bool(bool b)
+        {
+            return v1.Bool(b);
+        }
+
+        public A Iff(A b, A e1, A e2)
+        {
+            return v1.Iff(b, e1, e2);
+        }
+    }
+}
