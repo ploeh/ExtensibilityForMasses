b69ff4fe01b8b9f0de99eccaf2a03b517634e12b
Mark Seemann <mark@ploeh.dk>
2021-06-13 22:22:03 +0200

    Add batch factory from figure 13
    I let Visual Studio generate the Op class, but I'm making no attempt at
    guessing how it's defined, or if it's a class (versus an interface) at
    all...

---
+++ ExtensibilityForMasses/IBatchFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public interface IBatchFactory<E>
+    {
+        E Var(string name); // Variable reference
+        E Data(object value); // Simple constant (number, string, or date)
+        E Fun(string var, E body);
+        E Prim(Op op, IEnumerable<E> args); // Unary and binary operators
+        E Prop(E @base, string field); // Field access
+        E Assign(Op op, E target, E source); // Assignment
+        E Let(string var, E expression, E body); // Control flow
+        E If(E condition, E thenExp, E elseExp);
+        E Loop(Op op, string var, E collection, E body);
+        E Call(E target, string method, IEnumerable<E> args); // Method invocation
+        E In(string location); // Reading and writing forest
+        E Out(string location, E expression);
+    }
+}
