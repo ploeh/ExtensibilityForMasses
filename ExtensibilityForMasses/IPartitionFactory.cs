240fd1883f00204ba9be2e4edc60f46cd7c482fb
Mark Seemann <mark@ploeh.dk>
2021-06-14 09:54:56 +0200

    Add IPartitionFactory from figure 15

---
+++ ExtensibilityForMasses/IPartitionFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public interface IPartitionFactory<E> : IBatchFactory<E>
+    {
+        E Other(object external, params E[] subs);
+        E DynamicCall(E target, string method, IEnumerable<E> args);
+        E Mobile(string type, object obj, E exp);
+    }
+}
