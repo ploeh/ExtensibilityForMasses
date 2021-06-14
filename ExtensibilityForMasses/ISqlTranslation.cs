a6c260ccd57e0d6c91933b0b461b9aba09e7211e
Mark Seemann <mark@ploeh.dk>
2021-06-14 09:49:10 +0200

    Add SQL translation from figure 14
    As was also the case in the previous commit, I let Visual Studio create
    new types for the various parameters and return values implied by the
    interface.
    
    These examples aren't clearly explained, so I'll leave the code at that.
    I fail to find these examples helpful.

---
+++ ExtensibilityForMasses/ISqlTranslation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public interface ISqlTranslation
+    {
+        void ToSql(StringBuilder sb, IEnumerable<object> @params, Forest data);
+        Expression Normalize(ISchema schema, SqlQuery query,
+            Expression outerCond, Env env, NormType normType);
+        SqlTable GetTable();
+        Expression InvertPath(Expression e, Env env, bool fromChild);
+        SqlTable GetTableNoJoins(Env env);
+        SqlTable GetBase(Env env);
+        Expression WithoutTransformations();
+        Expression GetTransformations(Expression @base);
+        Expression TrimLast(Env env);
+    }
+}
---
+++ ExtensibilityForMasses/SqlQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Ploeh.Study.ExtensibilityForMasses
+{
+    public class SqlQuery
+    {
+    }
+}
